5c4c6a5eeaa2cb58831413a6aec29c8800840696
hhs920 <hhs9200@gmail.com>
2025-08-18 15:06:07 +0900

    OnItemBeginOverlap 캐스팅 수정

--- Source/CyberRush/Private/LJW/Item.cpp
+++ Source/CyberRush/Private/LJW/Item.cpp
@@ -6,6 +6,7 @@
 #include "CyberRushCharacter.h"
 #include "Components/BoxComponent.h"
 #include "GameFramework/RotatingMovementComponent.h"
+#include "HHS/RunnerPlayerBase.h"
 #include "LJW/FloorTile.h"
 
 // Sets default values
@@ -52,7 +53,7 @@ void AItem::Tick(float DeltaTime)
 
 void AItem::OnItemBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (auto Player = Cast<ACyberRushCharacter>(OtherActor))
+	if (auto Player = Cast<ARunnerPlayerBase>(OtherActor))
 	{
 		Destroy();
 	}
